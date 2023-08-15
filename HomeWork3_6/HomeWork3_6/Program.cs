edfdea8723a900d6e0f072659a9a9ca17317fcca
BogdanZhyck <zhyck.bogdan@gmail.com>
2023-08-15 19:12:04 +0300

    homework3_5

---
+++ HomeWork3_6/HomeWork3_6/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HomeWork3_6
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			MessageBox messageBox = new MessageBox();
+			messageBox.WindowClosed += MessageBox_WindowClosed;
+
+			messageBox.Open();
+
+			Console.ReadLine();
+		}
+
+		private static void MessageBox_WindowClosed(object sender, State state)
+		{
+			if (state == State.Ok)
+			{
+				Console.WriteLine("The operation has been confirmed");
+			}
+			else if (state == State.Cancel)
+			{
+				Console.WriteLine("The operation was rejected");
+			}
+		}
+	}
+}
