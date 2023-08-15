2d440caf21f29cbf28f05edb97a0e9c615f4a6e1
BogdanZhyck <zhyck.bogdan@gmail.com>
2023-08-15 18:53:54 +0300

    homework3_2

---
+++ HomeWork3_2/HomeWork3_2/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3_2
+{
+	public class Contact
+	{
+		public string Name { get; set; }
+		public string PhoneNumber { get; set; }
+	}
+}
---
+++ HomeWork3_2/HomeWork3_2/CustomContactCollection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3_2
+{
+	class CustomContactCollection
+	{
+		private Dictionary<char, List<Contact>> _contactsByAlphabet = new Dictionary<char, List<Contact>>();
+		private CultureInfo _currentCulture;
+
+		public CustomContactCollection(CultureInfo culture = null)
+		{
+			_currentCulture = culture ?? CultureInfo.GetCultureInfo("en-US");
+		}
+
+		public void AddContact(Contact contact)
+		{
+			char firstChar = GetFirstChar(contact.Name);
+			if (!_contactsByAlphabet.ContainsKey(firstChar))
+			{
+				_contactsByAlphabet[firstChar] = new List<Contact>();
+			}
+			_contactsByAlphabet[firstChar].Add(contact);
+		}
+
+		public List<Contact> GetContactsByAlphabet()
+		{
+			var sortedContacts = _contactsByAlphabet
+				 .OrderBy(pair => pair.Key)
+				 .ToDictionary(pair => pair.Key, pair => pair.Value);
+
+			return sortedContacts.SelectMany(pair => pair.Value).ToList();
+		}
+
+		private char GetFirstChar(string text)
+		{
+			var collator = StringInfo.GetTextElementEnumerator(text);
+			if (collator.MoveNext())
+			{
+				string firstElement = collator.GetTextElement();
+				return char.ToUpper(firstElement[0], _currentCulture);
+			}
+			return '#'; 
+		}
+	}
+}
